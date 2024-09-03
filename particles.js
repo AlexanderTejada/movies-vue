2f69c6ee9f8a85061152c47e9bd105b1895ef5ee
AlexanderTejada <120703301+AlexanderTejada@users.noreply.github.com>
2024-09-03 03:11:48 -0300

    AlEXFLIX

---
+++ particles.js
@@ -0,0 +1,74 @@
+particlesJS('particles-js', {
+    "particles": {
+        "number": {
+            "value": 30, // Reduce el número de partículas
+            "density": {
+                "enable": true,
+                "value_area": 1000 // Aumenta el área para dispersarlas más
+            }
+        },
+        "color": {
+            "value": "#ffffff" // Color de las partículas
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+            "polygon": {
+                "nb_sides": 5
+            }
+        },
+        "opacity": {
+            "value": 0.7, // Baja la opacidad para hacerlas más sutiles
+            "random": true, // Hacer la opacidad de cada partícula aleatoria
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 2, // Disminuye el tamaño de las partículas
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 20,
+                "size_min": 0.1,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": false // Desactiva las líneas entre partículas
+        },
+        "move": {
+            "enable": true,
+            "speed": 0.5, // Reduce la velocidad de movimiento para suavizar el efecto
+            "direction": "none",
+            "random": true,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": false // Desactiva la interacción con el mouse
+            },
+            "onclick": {
+                "enable": false // Desactiva la interacción con clic
+            },
+            "resize": true
+        }
+    },
+    "retina_detect": true
+});
